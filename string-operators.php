d36020e02e2c57a8419063fe0d59f5966f4889ed
razinahmed09 <raheenahmee@gmail.com>
2023-12-21 04:43:04 +0500

    ...

---
+++ string-operators.php
@@ -0,0 +1,20 @@
+<?php
+
+// $a = "hello";
+// $b = $a . "world" . "<br> this is my world";
+
+// echo  $b;
+
+
+//second method
+
+//  .= string operators
+
+$a = "hello ";
+$a .= "this is ";
+$a .= "empty file";
+
+
+echo $a;
+
+?>
